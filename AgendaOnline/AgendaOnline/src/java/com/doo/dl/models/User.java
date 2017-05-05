2e9679ca87390576b3ea8d2d8816a33e550dfdf1
lsti1727375 <josejulio2119@gmail.com>
2017-05-05 01:16:39 -0500

    Add files via upload

---
+++ AgendaOnline/AgendaOnline/src/java/com/doo/dl/models/User.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.doo.dl.models;
+
+/**
+ *
+ * @author SEG-14
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String name;
+    private String semestre;
+    private String matricula;
+    private String carrera;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the semestre
+     */
+    public String getSemestre() {
+        return semestre;
+    }
+    
+    public void setSemestre(String semestre){
+        this.semestre = semestre;
+      
+    }
+
+    /**
+     * @return the materia
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    /**
+     * @return the carrera
+     */
+    public String getCarrera() {
+        return carrera;
+    }
+
+    /**
+     * @param carrera the carrera to set
+     */
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+   
+}
